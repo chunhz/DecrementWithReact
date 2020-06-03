349ae5b6106b9afd85c61ce1c3198e5c8ae98173
Chunhao Zheng <chz062009@gmail.com>
2020-06-02 20:40:27 -0400

    files uploaded

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class Decrement extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      currentNum : props.start
+    }
+  }
+  decreNum = ()=>{
+    if (this.state.currentNum <= 0)
+    alert('Cannot be less than zero');
+    else
+    this.setState({currentNum: this.state.currentNum-1});
+  }
+  render(){
+    return <div>
+    <h1>Decrement: </h1>
+    <p id ="currentNum">Current Number is:   {this.state.currentNum}</p>
+    <button onClick={this.decreNum}>Decrement</button>
     </div>
-  );
+  }
+}
+
+class App extends Component{
+  render(){
+    return <div>
+      <Decrement start={5} />
+    </div>
+  }
 }
 
 export default App;
